f48ce6a49844fde55fee5a442e31bb084f28cc23
Maciek Swat <maciekswat@gmail.com>
2013-11-13 17:50:45 -0500

    fixing build scripts

--- compiler_farm_scripts/sharedVM/scripts/build_cc3d.sh
+++ compiler_farm_scripts/sharedVM/scripts/build_cc3d.sh
@@ -17,11 +17,12 @@ function run_and_watch_status {
 
 }
 
+export CC3D_BUILD_SCRIPTS_GIT_DIR=~/CC3D_BUILD_SCRIPTS_GIT_DIR
+eval CC3D_BUILD_SCRIPTS_GIT_DIR=$CC3D_BUILD_SCRIPTS_GIT_DIR
 
-
-time run_and_watch_status BUILDING_CC3D_370 ./build-cc3d-370-compiler-farm.sh
-time run_and_watch_status BUILDING_RR_LLVM  ./build-rr-llvm-compiler-farm.sh 
-time run_and_watch_status BUILDING_CC3D_371 ./build-cc3d-371-compiler-farm.sh
+time run_and_watch_status BUILDING_CC3D_370 ${CC3D_BUILD_SCRIPTS_GIT_DIR}/build-cc3d-370-compiler-farm.sh
+time run_and_watch_status BUILDING_RR_LLVM  ${CC3D_BUILD_SCRIPTS_GIT_DIR}/build-rr-llvm-compiler-farm.sh 
+time run_and_watch_status BUILDING_CC3D_371 ${CC3D_BUILD_SCRIPTS_GIT_DIR}/build-cc3d-371-compiler-farm.sh
 /sbin/poweroff
 
 
