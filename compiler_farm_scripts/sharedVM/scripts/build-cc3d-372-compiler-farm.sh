74cad1275e9c450454e775034086f2586252bc10
maciekswat <maciekswat@gmail.com>
2014-03-07 15:28:59 -0500

    added v372 scripts

---
+++ compiler_farm_scripts/sharedVM/scripts/build-cc3d-372-compiler-farm.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+function run_and_watch_status {
+    # first argument is a task descriptor and is mandatory
+    # the remaining arguments make up a command to execute
+    
+    #executing the command
+    "${@:2}"
+    # querrying its status
+    status=$?
+    echo "STATUS=$status"
+    if [ $status -ne 0 ]; then
+        echo "error with $1"
+        exit $status
+    fi
+    return $status    
+
+}
+
+export VERSION=3.7.2
+echo "THIS IS VERSION ${VERSION} "
+
+
+export CC3D_GIT_DIR=~/CC3D_GIT
+export CC3D_BINARIES_DIR=/media/sf_sharedVM/binaries/${VERSION}/linux
+eval CC3D_GIT_DIR=$CC3D_GIT_DIR
+export number_of_cpus=8
+export install_path=~/install_projects_${VERSION}/${VERSION}
+eval install_path=$install_path
+
+# get binaries repository
+# mkdir $CC3D_BINARIES_DIR
+# cd $CC3D_BINARIES_DIR
+# svn checkout http://www.compucell3d.org/BinDoc/cc3d_binaries/binaries/3.7.0/linux .
+# svn update
+
+export CC3D_BUILD_SCRIPTS_GIT_DIR=~/CC3D_BUILD_SCRIPTS_GIT
+eval CC3D_BUILD_SCRIPTS_GIT_DIR=$CC3D_BUILD_SCRIPTS_GIT_DIR
+
+cd $CC3D_BUILD_SCRIPTS_GIT_DIR
+git checkout master
+git pull
+
+cd $CC3D_GIT_DIR
+git checkout $VERSION
+git pull
+
+cd $CC3D_BUILD_SCRIPTS_GIT_DIR/linux/372
+
+time run_and_watch_status BUILDING_CC3D_372_COMPILER_FARM_SCRIPT  ./build-debian-cc3d-372.sh -s=$CC3D_GIT_DIR -p=$install_path -c=$number_of_cpus
+
+cd $CC3D_BUILD_SCRIPTS_GIT_DIR/linux/DebianPackageBuilder
+
+# remove old deb packages
+rm -rf ${install_path}_deb/*
+
+python ./deb-pkg-builder-372.py -d $install_path -i ${install_path}_deb -v ${VERSION}
+
+
+mkdir -p ${CC3D_BINARIES_DIR}
+#removing old debian packages
+# rm -rf ${CC3D_BINARIES_DIR}/* 
+
+cd ${install_path}_deb
+
+cp *.deb $CC3D_BINARIES_DIR
+
+
--- compiler_farm_scripts/sharedVM/scripts/build_cc3d.sh
+++ compiler_farm_scripts/sharedVM/scripts/build_cc3d.sh
@@ -29,9 +29,13 @@ function run_and_watch_status {
 export CC3D_BUILD_SCRIPTS_GIT_DIR=~/CC3D_BUILD_SCRIPTS_GIT
 eval CC3D_BUILD_SCRIPTS_GIT_DIR=$CC3D_BUILD_SCRIPTS_GIT_DIR
 
-time run_and_watch_status BUILDING_CC3D_370 ${CC3D_BUILD_SCRIPTS_GIT_DIR}/compiler_farm_scripts/sharedVM/scripts/build-cc3d-370-compiler-farm.sh
-time run_and_watch_status BUILDING_RR_LLVM  ${CC3D_BUILD_SCRIPTS_GIT_DIR}/compiler_farm_scripts/sharedVM/scripts/build-rr-llvm-compiler-farm.sh 
-time run_and_watch_status BUILDING_CC3D_371 ${CC3D_BUILD_SCRIPTS_GIT_DIR}/compiler_farm_scripts/sharedVM/scripts/build-cc3d-371-compiler-farm.sh
+# time run_and_watch_status BUILDING_CC3D_370 ${CC3D_BUILD_SCRIPTS_GIT_DIR}/compiler_farm_scripts/sharedVM/scripts/build-cc3d-370-compiler-farm.sh
+# time run_and_watch_status BUILDING_RR_LLVM  ${CC3D_BUILD_SCRIPTS_GIT_DIR}/compiler_farm_scripts/sharedVM/scripts/build-rr-llvm-compiler-farm.sh 
+# time run_and_watch_status BUILDING_CC3D_371 ${CC3D_BUILD_SCRIPTS_GIT_DIR}/compiler_farm_scripts/sharedVM/scripts/build-cc3d-371-compiler-farm.sh
+
+# time run_and_watch_status BUILDING_RR_LLVM  ${CC3D_BUILD_SCRIPTS_GIT_DIR}/compiler_farm_scripts/sharedVM/scripts/build-rr-llvm-compiler-farm.sh 
+time run_and_watch_status BUILDING_CC3D_371 ${CC3D_BUILD_SCRIPTS_GIT_DIR}/compiler_farm_scripts/sharedVM/scripts/build-cc3d-372-compiler-farm.sh
+
 /sbin/poweroff
 
 
