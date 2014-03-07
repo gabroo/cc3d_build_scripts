5eb846fead7dc41560c2bcccd1e2a5a5b6c12aaa
maciekswat <maciekswat@gmail.com>
2014-03-07 15:51:58 -0500

    updated build_cc3d.sh

--- compiler_farm_scripts/sharedVM/scripts/build_cc3d.sh
+++ compiler_farm_scripts/sharedVM/scripts/build_cc3d.sh
@@ -29,13 +29,15 @@ function run_and_watch_status {
 export CC3D_BUILD_SCRIPTS_GIT_DIR=~/CC3D_BUILD_SCRIPTS_GIT
 eval CC3D_BUILD_SCRIPTS_GIT_DIR=$CC3D_BUILD_SCRIPTS_GIT_DIR
 
+cd $CC3D_BUILD_SCRIPTS_GIT_DIR
+git pull
+
 # time run_and_watch_status BUILDING_CC3D_370 ${CC3D_BUILD_SCRIPTS_GIT_DIR}/compiler_farm_scripts/sharedVM/scripts/build-cc3d-370-compiler-farm.sh
 # time run_and_watch_status BUILDING_RR_LLVM  ${CC3D_BUILD_SCRIPTS_GIT_DIR}/compiler_farm_scripts/sharedVM/scripts/build-rr-llvm-compiler-farm.sh 
 # time run_and_watch_status BUILDING_CC3D_371 ${CC3D_BUILD_SCRIPTS_GIT_DIR}/compiler_farm_scripts/sharedVM/scripts/build-cc3d-371-compiler-farm.sh
 
-# time run_and_watch_status BUILDING_RR_LLVM  ${CC3D_BUILD_SCRIPTS_GIT_DIR}/compiler_farm_scripts/sharedVM/scripts/build-rr-llvm-compiler-farm.sh 
+time run_and_watch_status BUILDING_RR_LLVM  ${CC3D_BUILD_SCRIPTS_GIT_DIR}/compiler_farm_scripts/sharedVM/scripts/build-rr-llvm-compiler-farm.sh 
 time run_and_watch_status BUILDING_CC3D_371 ${CC3D_BUILD_SCRIPTS_GIT_DIR}/compiler_farm_scripts/sharedVM/scripts/build-cc3d-372-compiler-farm.sh
 
-/sbin/poweroff
-
+# /sbin/poweroff
 
