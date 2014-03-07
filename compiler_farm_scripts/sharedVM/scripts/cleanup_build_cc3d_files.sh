c182bbd562b8e3d5a7897841a22cb526d1099082
maciekswat <maciekswat@gmail.com>
2014-03-07 17:45:32 -0500

    simplified cleanup script

--- compiler_farm_scripts/sharedVM/scripts/cleanup_build_cc3d_files.sh
+++ compiler_farm_scripts/sharedVM/scripts/cleanup_build_cc3d_files.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
-export VERSION=3.7.0
-export VERSION1=3.7.1
-export VERSION2=3.7.2
-export VERSION3=RR_LLVM
-echo "THIS IS VERSION ${VERSION} "
 
 
-export CC3D_GIT_DIR=~/CC3D_GIT
-export CC3D_BINARIES_DIR=/media/sf_sharedVM/binaries/${VERSION}/linux
-eval CC3D_GIT_DIR=$CC3D_GIT_DIR
-export number_of_cpus=8
-export install_path=~/install_projects_${VERSION}/${VERSION}
-eval install_path=$install_path
-
-
-rm -rf ~/install_projects_${VERSION}
-rm -rf ~/install_projects_${VERSION1}
-rm -rf ~/install_projects_${VERSION2}
-rm -rf ~/install_projects_${VERSION3}
+rm -rf ~/install_projects_3.7.0
+rm -rf ~/install_projects_3.7.1
+rm -rf ~/install_projects_3.7.2
+rm -rf ~/install_projects_RR_LLVM/build/RR
+rm -rf ~/install_projects_RR_LLVM/RR_LLVM
