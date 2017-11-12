ace33150c49487124a694199fb10da1c39e87e33
compucell3d <compucell3dproject@gmail.com>
2017-11-11 23:19:29 -0500

    fixed build scripts for windows

--- windows/build_scripts/build_all.bat
+++ windows/build_scripts/build_all.bat
@@ -4,19 +4,19 @@
 
 @set version=3.7.7
 
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
+REM call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
 
-c:\miniconda64\python win_cc3d_builder.py  -p D:/install_projects/%version%-64bit -s D:/CC3D_GIT  -i D:/CC3D_FILES_SVN/binaries/%version%/windows -v %version%.0 --config=config_64bit.json
+REM c:\miniconda64\python win_cc3d_builder.py  -p D:/install_projects/%version%-64bit -s D:/CC3D_GIT  -i D:/CC3D_FILES_SVN/binaries/%version%/windows -v %version%.0 --config=config_64bit.json
 
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
+REM call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
 
-c:\miniconda64\python .\win_cc3d_builder.py  -p D:/install_projects/%version%-64bit-gpu -s D:/CC3D_GIT  -i D:/CC3D_FILES_SVN/binaries/%version%/windows -v %version%.0 --config=config_64bit.json --gpu
+REM c:\miniconda64\python .\win_cc3d_builder.py  -p D:/install_projects/%version%-64bit-gpu -s D:/CC3D_GIT  -i D:/CC3D_FILES_SVN/binaries/%version%/windows -v %version%.0 --config=config_64bit.json --gpu
 
-REM call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
 
-REM c:\miniconda64\python .\win_cc3d_builder.py  -p D:/install_projects/%version%-32bit -s D:/CC3D_GIT  -i D:/CC3D_FILES_SVN/binaries/%version%/windows -v %version%.0 --config=config_32bit.json
+c:\miniconda64\python .\win_cc3d_builder.py  -p D:/install_projects/%version%-32bit -s D:/CC3D_GIT  -i D:/CC3D_FILES_SVN/binaries/%version%/windows -v %version%.0 --config=config_32bit.json
 
-REM call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
 
-REM c:\miniconda64\python .\win_cc3d_builder.py  -p D:/install_projects/%version%-32bit-gpu -s D:/CC3D_GIT  -i D:/CC3D_FILES_SVN/binaries/%version%/windows -v %version%.0 --config=config_32bit.json --gpu
+c:\miniconda64\python .\win_cc3d_builder.py  -p D:/install_projects/%version%-32bit-gpu -s D:/CC3D_GIT  -i D:/CC3D_FILES_SVN/binaries/%version%/windows -v %version%.0 --config=config_32bit.json --gpu
 
