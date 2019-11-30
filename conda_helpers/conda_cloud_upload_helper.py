e3e142ffd310152c35169208a57c6de9c889a07f
Maciek Swat <maciekswat@gmail.com>
2019-11-30 15:28:49 -0500

    added instructions for archiving conda packages for OSX. expanded conda_cloud_upload_helper.py script to include better instructions and rudimentary sanity check for installed packages

--- conda_helpers/conda_cloud_upload_helper.py
+++ conda_helpers/conda_cloud_upload_helper.py
@@ -1,13 +1,46 @@
 """
 This program creates uploader script that you can use to upload conda packages that are cached on your computer.
+
+The workflow is as follows:
+1. Create explicit environment file for conda environment file you want to archive
+for example - if our environment is called cc3d_2019 we woudl do the following:
+
+conda activate cc3d_2019
+conda list --explicit > <work_dir>/cc3d_2019_env.txt
+
+2. Once you have this file you run this script as follows:
+
+python conda_cloud_upload_helper.py --conda-env-file=<work_dir>/cc3d_2019_env.txt --work-dir=<work_dir>
+--conda-install-path=<conda_install_path> --channel-name=compucell3d
+
+HEre is an example command I ran on OSX:
+
+python conda_cloud_upload_helper.py
+--conda-env-file=/Users/m/CC3D_BUILD_SCRIPTS_GIT/mac/conda_upload_workdir/cc3d_2019_env.txt
+--work-dir=/Users/m/CC3D_BUILD_SCRIPTS_GIT/mac/conda_upload_workdir
+--conda-install-path=/Users/m/miniconda3
+--channel-name=compucell3d
+
 To run the script you first need to login to anaconda cloud by running
 
 anaconda login
 
-then you run created script and things will get uploaded
+Make sure that anaconda-client is installed. If it is not you will need to run
+
+conda install anaconda-client
+
+and then try anaconda login
+
+then you run created script and things will get uploaded. In my case on OSX i did the following
+
+cd /Users/m/CC3D_BUILD_SCRIPTS_GIT/mac/conda_upload_workdir
+chmod +x conda_upload.sh
+./conda_upload.sh
 
 """
 import argparse
+from collections import OrderedDict
+import pandas as pd
 from pathlib import Path
 import sys
 
@@ -75,16 +108,17 @@ def parse_conda_env(conda_env_file: str) -> dict:
     return parse_dict
 
 
-def create_upload_script(parse_dict: dict, work_dir: str, conda_install_path: str):
+def create_upload_script(parse_dict: dict, work_dir: str, conda_install_path: str)->pd.DataFrame:
     """
     Generates bash or batch script (inside work_dir) that will upload packages.
 
     :param parse_dict:
     :param work_dir:
     :param conda_install_path:
-    :return:
+    :return: dataframe with full package filenames and a file_exists column
     """
-
+    package_files_to_upload = []
+    package_exists = []
     Path(work_dir).mkdir(parents=True, exist_ok=True)
 
     if sys.platform.startswith('win'):
@@ -96,6 +130,20 @@ def create_upload_script(parse_dict: dict, work_dir: str, conda_install_path: st
         for pkg_name in parse_dict['pkg_names']:
             full_pkg_path = str(Path(conda_install_path).joinpath('pkgs', pkg_name))
             sc_out.write(f'anaconda upload {full_pkg_path}\n')
+            package_files_to_upload.append(full_pkg_path)
+            package_exists.append(Path(full_pkg_path).exists())
+
+    status_df = pd.DataFrame(
+        OrderedDict(
+            [
+                ('package_path', package_files_to_upload),
+                ('exists',package_exists)
+            ]
+        )
+
+    )
+
+    return status_df
 
 
 def create_modified_environment(parse_dict: dict, work_dir: str, environment_basename: str = 'conda_env.txt',
@@ -141,13 +189,21 @@ if __name__ == '__main__':
 
     parse_dict = parse_conda_env(conda_env_file=conda_env_file)
 
-    create_upload_script(parse_dict=parse_dict, work_dir=work_dir, conda_install_path=conda_install_path)
+    status_df = create_upload_script(parse_dict=parse_dict, work_dir=work_dir, conda_install_path=conda_install_path)
 
     modified_conda_env = Path(conda_env_file).stem
 
     create_modified_environment(parse_dict=parse_dict, work_dir=work_dir, environment_basename=modified_conda_env,
                                 channel_name=channel_name)
 
+    status_df.to_csv(str(Path(work_dir).joinpath('package_status.csv')), index=False)
 
-
+    # sanity check output
+    if sum(~status_df.exists):
+        print('The following packages are required to recreate environment but are not found in the conda cache folder:')
+        print('\n'.join(list(status_df[~status_df.exists].package_path.values)))
+        print('You may still go to anaconda cloud and download those packages manually and place them in conda'
+              'cache folder')
+    else:
+        print('All packages exist')
 
