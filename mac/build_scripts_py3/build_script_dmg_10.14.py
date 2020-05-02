43e79ec95017af14e4f53649ca4d0b13d2bcb163
Maciek Swat <maciekswat@gmail.com>
2020-05-02 16:26:18 -0600

    updated example build command for OSX

--- mac/build_scripts_py3/build_script_dmg_10.14.py
+++ mac/build_scripts_py3/build_script_dmg_10.14.py
@@ -18,6 +18,10 @@ example commands:
 
 python /Users/m/CC3D_BUILD_SCRIPTS_GIT/mac/build_scripts_py3/build_script_dmg_10.14.py -p /Users/m/install_projects/CC3D_4.2.0 -s /Users/m/CC3D_PY3_GIT   -v 4.2.0.0 --config=mac/build_scripts_py3/config_64bit_2021_10.14.json -c 8
 
+follow with
+
+python /Users/m/CC3D_BUILD_SCRIPTS_GIT/mac/build_scripts_py3/rpath_handlers/finalize_cc3d_install.py --cc3d-install-dir=/Users/m/install_projects/CC3D_4.2.0 --certificate-label="Developer ID Application: Indiana University (5J69S77A7G)" --python-source-signed-dir=/Users/m/prerequisites/4.1.2_10.14/python37_signed_entitlements --keychain-path=/Users/m/Library/Keychains/login.keychain-db
+
 """
 import time
 from os.path import *
